fcb673fde95fa2f7e7b80a8aa4a14fcf57d92a6f
Vera Nouaime <veranouaime@veras-air.lan>
2023-06-25 17:09:17 -0700

    Completed python-ds-exercises

---
+++ 11_flip_case/flip_case.py
@@ -0,0 +1,24 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    to_swap = to_swap.lower()
+    new_phrase = ""
+
+    for ltr in phrase:
+        if ltr.lower() == to_swap:
+            ltr = ltr.swapcase()
+        new_phrase += ltr
+
+    return new_phrase
+
+
